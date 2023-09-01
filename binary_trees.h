bf7bd48ca85075bd1e71787363973ca37ac3440d
beekaz <sooperfect00@gmail.com>
2023-09-01 16:48:23 -0700

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -62,5 +62,7 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
+int is_complete(const binary_tree_t *tree, int index, int size);
+
 
 #endif
