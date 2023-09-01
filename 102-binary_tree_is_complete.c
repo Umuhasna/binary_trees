fbfcaff3461553e8f7ccebe09e18f14359924bc5
beekaz <sooperfect00@gmail.com>
2023-09-01 16:33:40 -0700

    a function that checks if a binary tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -8,17 +8,15 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (is_complete(tree, 0, binary_tree_size(tree)) == 1)
-        return (1);
-
-    return (0);
+	return (is_complete(tree, 0, binary_tree_size(tree)));
 }
 
 /**
- * is_complete - Recursive helper function to check if a binary tree is complete.
+ * is_complete - Recursive helper function
+ * to check if a binary tree is complete.
  * @tree: Pointer to the current node.
  * @index: Index of the current node.
  * @size: Total size of the binary tree.
@@ -27,14 +25,14 @@ int binary_tree_is_complete(const binary_tree_t *tree)
  */
 int is_complete(const binary_tree_t *tree, int index, int size)
 {
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (index >= size)
-        return (0);
+	if (index >= size)
+		return (0);
 
-    return (is_complete(tree->left, 2 * index + 1, size) &&
-            is_complete(tree->right, 2 * index + 2, size));
+	return (is_complete(tree->left, 2 * index + 1, size) &&
+			is_complete(tree->right, 2 * index + 2, size));
 }
 
 /**
@@ -45,8 +43,7 @@ int is_complete(const binary_tree_t *tree, int index, int size)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
