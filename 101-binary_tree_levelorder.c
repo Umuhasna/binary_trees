5aced432732482f3d238e4f33cc6f8c8e2a41184
beekaz <sooperfect00@gmail.com>
2023-09-01 16:10:09 -0700

    a function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -14,7 +14,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	/* Create a queue using an array */
-	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024)
+	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024);
 		if (queue == NULL)
 			return;
 
